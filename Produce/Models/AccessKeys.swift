7ff5f7088f384b3ef0a5006a0aab08a8ba2adf8c
Garrett Oreilly <garrettoreilly@icloud.com>
2018-06-22 20:30:09 -0400

    Reorganized files

---
+++ Produce/Models/AccessKeys.swift
@@ -0,0 +1,13 @@
+//
+//  AccessKeys.swift
+//  Produce
+//
+//  Created by Garrett Oreilly on 2018-06-22.
+//  Copyright © 2018 Garrett Oreilly. All rights reserved.
+//
+
+import Foundation
+
+struct AccessKeys {
+    static let newsAPI = "f62ff54669e445d6afd4650128bba561"
+}
